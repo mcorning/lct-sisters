a4af59f8bf378986dbe3f0e79e97e53f2cdf2f87
Michael Corning <michael@secours.io>
2021-04-28 12:56:27 -0700

    basic install ready for heroku

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
-
+let color = 'purple';
 export default new Vuetify({
-  icons: { iconfont: 'md' },
+  theme: {
+    themes: {
+      light: {
+        primary: colors[color].darken2,
+        secondary: colors[color].lighten3,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
 });
