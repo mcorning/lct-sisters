1a29d727603021a0b99c67be94ccb4353fffd052
Michael Corning <michael@secours.io>
2021-09-14 21:04:29 -0700

    remove secrets

--- redisGraph.options.js
+++
@@ -1,9 +0,0 @@
-// https://app.redislabs.com/#/bdb/tabs/conf/10283899
-// lctgraph
-
-module.exports = {
-  graphName: process.env.NODE_ENV == 'production' ? 'Sisters' : 'Sandbox',
-  redisHost: 'redis-16914.c53.west-us.azure.cloud.redislabs.com',
-  redisPassword: 'kqhiYfB2XwoYV2Jy3vUw3eXDrWhCaSWq',
-  redisPort: 16914,
-};
