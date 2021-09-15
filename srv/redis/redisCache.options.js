1fd2a7c6bc33bd1fa4e12d2ab8bd8ceea40c6009
Michael Corning <michael@secours.io>
2021-09-15 11:56:54 -0700

    Deleted file from repository only

--- srv/redis/redisCache.options.js
+++
@@ -1,9 +0,0 @@
-// source: https://app.redislabs.com/#/bdb/tabs/conf/10303198
-// removing from repo
-const options = {
-  redisCacheHost: 'redis-11659.c60.us-west-1-2.ec2.cloud.redislabs.com',
-  redisCachePassword: 'anzq4lNE6FT2VfW7YiAcdFDgzasg2heY',
-  redisCachePort: 11659,
-};
-
-module.exports = options;
