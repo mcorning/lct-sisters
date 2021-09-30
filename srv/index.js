9ed28fd2fe3b3f194fc3c2690e863c7115eca394
Michael Corning <michael@secours.io>
2021-09-30 14:30:48 -0700

    refactoring servers to use rediscache

--- srv/index.js
+++ srv/index.js
@@ -85,137 +85,6 @@ const server = express()
   });
 const io = socketIO(server);
 
-// function report(sessionID, userID, username) {
-//   console.log(info('2) Middleware handling socket:'));
-//   console.log(
-//     info(
-//       'sessionID:',
-//       sessionID || 'No stored session',
-//       '\t',
-//       'userID:',
-//       userID || 'No userID',
-//       '\t',
-//       'username:',
-//       username || 'No username'
-//     )
-//   );
-// }
-//#endregion
-
-// function getSession(data) {
-//   const { sessionID, socket, session, next } = data;
-//   console.groupCollapsed(info('3) getSession(): data:', sessionID));
-//   if (!session) {
-//     // so we can create a session
-//     console.log('\t Going to create a session now...');
-//     console.groupEnd;
-//     //return;
-//     next();
-//   }
-
-//   const { userID, username } = session;
-
-//   // TODO what if this isn't true?
-//   if (session && userID && sessionID) {
-//     console.log(getNow());
-//     console.log(info(`4) Handshake: Known party: ${username}`));
-//     // if we have seen this session before, ensure the client uses the same
-//     // userID and username used in the last session
-//     console.log(info(printJson(session)));
-//     console.groupEnd;
-
-//     socket.sessionID = sessionID;
-//     socket.userID = userID;
-//     socket.username = username;
-
-//     console.log(
-//       success(`5) LEAVING io.use() with  ${sessionID}'s session data.`)
-//     );
-//     return;
-//   }
-// }
-
-// function createSession(data) {
-//   // otherwise, setup the new user...
-//   const { socket, next, username } = data;
-//   const { sessionID, userID } = socket.handshake.auth;
-//   console.groupCollapsed(
-//     '4) createSession(): username/auth',
-//     data.unsername,
-//     printJson(socket.handshake.auth)
-//   );
-
-//   // do we need to create a new session?
-//   if ((socket.sessionID || sessionID) && (socket.userID || userID)) {
-//     // not if we have IDs
-//     console.groupEnd;
-//     return next();
-//   }
-
-//   console.log('\n', info(new Date().toLocaleString()));
-//   console.log(warn('5) Handshake: Unknown party'));
-//   console.log(warn(`\tAssigning new sessionID and userID for ${username}`));
-
-//   //...with a userID, and a sessionID
-//   socket.sessionID = randomId(); // these values gets attached to the socket so the client knows which session has their data and messages
-//   socket.userID = randomId();
-//   socket.username = username; // username is fixed by client
-
-//   console.log(success('Leaving io.use()'));
-//   // handle the connection where we cache and return the session data to client for local storage.
-//   console.groupEnd;
-
-//   return next();
-// }
-
-// io.use((socket, next) => {
-//   console.log('1) Entering getSession middleware');
-//   const { sessionID } = socket.handshake.auth;
-//   cache.get('sessions', '_' + sessionID).then((session) => {
-//     if (!session) {
-//       console.log('Creating session');
-//       socket.sessionID = randomId(); // these values gets attached to the socket so the client knows which session has their data and messages
-//       socket.userID = randomId();
-//     }
-//     next();
-//   });
-// });
-
-// io.use((socket, next) => {
-//   console.log(
-//     '========================== io.use ==================================='
-//   );
-//   console.log('1) Entering io.use()');
-//   const { sessionID, userID, username } = socket.handshake.auth;
-//   console.assert(username, 'Expected a username (or usernumber). Got null.');
-//   // if first connection, prompt client for a username
-//   if (!username) {
-//     return next(new Error('No username'));
-//   }
-
-//   report(sessionID, userID, username);
-
-//   // see if we have a session for the username
-//   // cache
-//   //   .get('sessions', '_' + sessionID)
-//   //   .then((session) =>
-//   //     getSession({
-//   //       sessionID,
-//   //       socket,
-//   //       session,
-//   //       next,
-//   //     })
-//   //   )
-//   //   .then(() =>
-//   //     createSession({
-//   //       socket,
-//   //       username,
-//   //       next,
-//   //     })
-//   //   )
-//   //   .catch((e) => console.log(err('Error in cache:', e)));
-// });
-
 io.on('connection', (socket) => {
   const {
     socketID,
@@ -224,11 +93,7 @@ io.on('connection', (socket) => {
     username,
     usernumber,
   } = socket.handshake.auth;
-  // if (!userID) {
-  //   console.log(err('NO USERID! How did the code get this far?'));
-  //   return;
-  // }  // const { id: socketID, sessionID, userID } = socket;
-  // const username = socket.handshake.username;
+
   console.log(sessionID, userID, username, usernumber);
   const newSessionID = sessionID || randomId(); // these values gets attached to the socket so the client knows which session has their data and messages
   const newUserID = userID || randomId();
@@ -309,6 +174,41 @@ io.on('connection', (socket) => {
 
     // do all connected visitors handle this event? i find no alertPending event handlers
     // socket.broadcast.emit('alertPending', riskScore);
+
+    // alerts is an array of userIDs
+    const alertOthers = (socket, alerts, riskScore) => {
+      const msPerDay = 1000 * 60 * 60 * 24;
+
+      const sendExposureAlert = (to, riskScore) => {
+        console.log('Alerting:', to); // to is a userID (of the exposed visitor)
+        socket.to(to).emit(
+          'exposureAlert',
+          riskScore //,
+          // ack((socketID) => {
+          //   console.log(success(socketID, 'confirms'));
+          // })
+        );
+      };
+
+      alerts.forEach((to) => {
+        if (io.sockets.adapter.rooms.has(to)) {
+          sendExposureAlert(to, riskScore);
+          alertsCache.delete(to);
+        } else {
+          alertsCache.set(to, {
+            cached: new Date(),
+          });
+        }
+      });
+
+      alertsCache
+        .purge((firstDate) => {
+          (Date.now() - new Date(firstDate).getTime()) / msPerDay > 14;
+        })
+        .then((purged) => console.log('Purged alertsCache of', purged))
+        .catch((err) => console.log('Purge alertsCache error:', err));
+    };
+
     const userID = socket.handshake.auth.userID;
     onExposureWarning(userID)
       .then((exposed) => {
@@ -322,13 +222,6 @@ io.on('connection', (socket) => {
       .catch((error) => console.error(error));
   });
 
-  socket.on('manageAppointment', (data) => {
-    console.log(getNow());
-
-    console.log(highlight(printJson(data)));
-  });
-
-  // socket.on('logVisit', (data, ack) => {
   socket.on('logVisit', (data, ack) => {
     // call the graph
     console.log(getNow());
@@ -392,45 +285,6 @@ io.on('connection', (socket) => {
     console.error('Incoming client_error!');
     errorCache.print(null, 'Errors:');
     console.error('See the errors.json later for details.');
-  });
-  //#endregion
-  socket.on('testGraph', (payload, ack) => {
-    const {query, param}=payload
-    testGraph({ query, param }, ack);
-  });
-
-  //#region Graph testing
-  socket.on('testGraphX', (query, ack) => {
-    // call the graph
-    console.log(getNow());
-    console.log(ack);
-    console.log(highlight('Visit to log:', printJson(query)));
-
-    function handleAck(results) {
-      if (ack) {
-        const x = results || 'no results';
-        console.log(highlight('acknowledging client', printJson(x)));
-        ack(results);
-        return;
-      }
-      console.log('No ack()');
-    }
-
-    // delegate to redis/redis.js
-    testGraph(query)
-      .toEither()
-      // TODO all inspect() to either-async
-      .map((x) => {
-        console.log(info('testGraph():', x));
-        return x;
-      })
-      .cata({
-        // ok: (results) => socket.emit('visitLogged', results),
-        ok: (results) => handleAck(results),
-        error: (results) => {
-          console.log(results, 'Issues calling redis.logVisit()');
-        },
-      });
   });
   //#endregion
 
@@ -504,49 +358,46 @@ io.on('connection', (socket) => {
       });
   });
   //#endregion
-});
 
-// alerts is an array of userIDs
-// const alertOthers = (socket, alerts, reason, ack) => {
-const alertOthers = (socket, alerts, riskScore) => {
-  const msPerDay = 1000 * 60 * 60 * 24;
-
-  const sendExposureAlert = (to, riskScore) => {
-    console.log('Alerting:', to); // to is a userID (of the exposed visitor)
-    socket.to(to).emit(
-      'exposureAlert',
-      riskScore //,
-      // ack((socketID) => {
-      //   console.log(success(socketID, 'confirms'));
-      // })
-    );
-  };
-
-  alerts.forEach((to) => {
-    if (io.sockets.adapter.rooms.has(to)) {
-      sendExposureAlert(to, riskScore);
-      alertsCache.delete(to);
-    } else {
-      alertsCache.set(to, { cached: new Date() });
-    }
+  //#region Graph testing
+  socket.on('testGraph', (payload, ack) => {
+    const { query, param } = payload;
+    testGraph({ query, param }, ack);
   });
 
-  alertsCache
-    .purge((firstDate) => {
-      (Date.now() - new Date(firstDate).getTime()) / msPerDay > 14;
-    })
-    .then((purged) => console.log('Purged alertsCache of', purged))
-    .catch((err) => console.log('Purge alertsCache error:', err));
+  socket.on('testGraphX', (query, ack) => {
+    // call the graph
+    console.log(getNow());
+    console.log(ack);
+    console.log(highlight('Visit to log:', printJson(query)));
 
-  // const ackWarning = (results, ack) => {
-  //   const ret = `Alerting ${results._resultsCount} other visitors.`;
-  //   if (results._resultsCount == 0) {
-  //     if (ack) {
-  //       ack(ret);
-  //     }
-  //     return;
-  //   }
-  //   return results;
-  // };
-};
+    function handleAck(results) {
+      if (ack) {
+        const x = results || 'no results';
+        console.log(highlight('acknowledging client', printJson(x)));
+        ack(results);
+        return;
+      }
+      console.log('No ack()');
+    }
+
+    // delegate to redis/redis.js
+    testGraph(query)
+      .toEither()
+      // TODO all inspect() to either-async
+      .map((x) => {
+        console.log(info('testGraph():', x));
+        return x;
+      })
+      .cata({
+        // ok: (results) => socket.emit('visitLogged', results),
+        ok: (results) => handleAck(results),
+        error: (results) => {
+          console.log(results, 'Issues calling redis.logVisit()');
+        },
+      });
+  });
+  //#endregion
+});
+
 //
